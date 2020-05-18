7ef8e141d403281c30b19820f5261fd41e13ce53
Eric Burns <eburns000@byui.edu>
2020-05-18 11:05:18 -0700

    copy lesson4 to lesson5

---
+++ lesson5/js/script.js
@@ -0,0 +1,29 @@
+// script.js: script file for shermanpeakweather.com site
+
+function wrapper() {
+
+// get date document was last modified and update datetime span
+const dt = new Date(document.lastModified);
+const dateOptions = {
+   weekday: 'long',
+   year: 'numeric',
+   month: 'long',
+   day: '2-digit'
+};
+document.getElementById("datetime").textContent = dt.toLocaleDateString('en-GB', dateOptions);
+
+// get current year and update currentyear span
+const cy = new Date();
+document.getElementById("currentyear").textContent = cy.toLocaleString('en-US', {
+   year: 'numeric'
+});
+
+// toggle hamburger menu
+const hambutton = document.querySelector('.ham');
+const mainnav = document.querySelector('.main-nav');
+
+hambutton.addEventListener('click', () => {
+   mainnav.classList.toggle('responsive')
+}, false);
+
+}
