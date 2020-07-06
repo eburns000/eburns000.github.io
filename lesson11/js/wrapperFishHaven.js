48e82d49d41be25b39591759b02c70b5bcbfdf8d
Eric Burns <eburns000@byui.edu>
2020-07-06 07:23:09 -0700

    fix sodasprings and fishhaven wrappers

--- lesson11/js/wrapperFishHaven.js
+++ lesson11/js/wrapperFishHaven.js
@@ -3,9 +3,6 @@ function wrapper() {
    // get date and time for footer - footerDateTime.js
    footerDateTime();
 
-   // banner message pancake breakfast in Preston - bannerMsg.js
-   postBanner();
-
    // set hamburger menu listener - toggleHamburger.js
    toggleHamburger();
 
