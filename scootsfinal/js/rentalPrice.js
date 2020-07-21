086468dfe2b6025ee1f45116df3b172b25859b69
eburns000 <eburns000@byui.edu>
2020-07-20 23:33:41 -0700

    work on rentals json functionality

---
+++ scootsfinal/js/rentalPrice.js
@@ -0,0 +1,43 @@
+function loadJSON(callback) {
+
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'rentalprices.json', true);
+    xobj.onreadystatechange = function () {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+            callback(xobj.responseText);
+        }
+    };
+    xobj.send(null);
+}
+
+function loadRentalPrices() {
+
+    // get array of rental price ul elements
+    rentalPriceLists = document.getElementsByClassName("rental-price");
+    console.log(rentalPriceLists);
+
+    // const jsonFile = 'rentalprices.json';
+
+
+    // fetch(jsonFile)
+    //     .then(function (response) {
+    //         return response.json();
+    //     })
+    //     .then(function (jsonObject) {
+
+    //         const rentalPriceData = jsonObject['rentalPrices'];
+
+    //         console.log(rentalPriceData);
+
+
+    //     });
+
+
+    loadJSON(function (response) {
+        // Parse JSON string into object
+        let actual_JSON = JSON.parse(response);
+        console.log(actual_JSON);
+    });
+
+}
