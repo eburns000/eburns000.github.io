a89652c05ec1a3def77f2fb4d2df3f7d9be5fec1
eburns000 <eburns000@byui.edu>
2020-07-21 18:56:11 -0700

    update load rental prices js

--- scootsfinal/js/rentalPrice.js
+++ scootsfinal/js/rentalPrice.js
@@ -1,15 +1,15 @@
-function loadJSON(callback) {
+// function loadJSON(callback) {
 
-    var xobj = new XMLHttpRequest();
-    xobj.overrideMimeType("application/json");
-    xobj.open('GET', 'rentalprices.json', true);
-    xobj.onreadystatechange = function () {
-        if (xobj.readyState == 4 && xobj.status == "200") {
-            callback(xobj.responseText);
-        }
-    };
-    xobj.send(null);
-}
+//     var xobj = new XMLHttpRequest();
+//     xobj.overrideMimeType("application/json");
+//     xobj.open('GET', 'rentalprices.json', true);
+//     xobj.onreadystatechange = function () {
+//         if (xobj.readyState == 4 && xobj.status == "200") {
+//             callback(xobj.responseText);
+//         }
+//     };
+//     xobj.send(null);
+// }
 
 function loadRentalPrices() {
 
@@ -17,27 +17,25 @@ function loadRentalPrices() {
     rentalPriceLists = document.getElementsByClassName("rental-price");
     console.log(rentalPriceLists);
 
-    // const jsonFile = 'rentalprices.json';
+    const jsonFile = 'https://eburns000.github.io/scootsfinal/data/rentalprices.json';
+
+    fetch(jsonFile)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (jsonObject) {
+
+            const rentalPriceData = jsonObject['rentalPrices'];
+
+            console.log(rentalPriceData);
+
+        });
 
 
-    // fetch(jsonFile)
-    //     .then(function (response) {
-    //         return response.json();
-    //     })
-    //     .then(function (jsonObject) {
-
-    //         const rentalPriceData = jsonObject['rentalPrices'];
-
-    //         console.log(rentalPriceData);
-
-
-    //     });
-
-
-    loadJSON(function (response) {
-        // Parse JSON string into object
-        let actual_JSON = JSON.parse(response);
-        console.log(actual_JSON);
-    });
+    // loadJSON(function (response) {
+    //     // Parse JSON string into object
+    //     let actual_JSON = JSON.parse(response);
+    //     console.log(actual_JSON);
+    // });
 
 }
