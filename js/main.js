9916f8704a5c58a416f2a3d50ad69bb15618c0e8
Eric Burns <eburns000@byui.edu>
2021-01-08 19:11:18 -0800

    add wdd330 note template structure

--- js/main.js
+++ js/main.js
@@ -1,24 +1,12 @@
 // array of links
 const links = [{
-    label: "Week 01 Notes",
+    label: "Week 01 Notes & Exercises",
     url: "wdd330/week01/index.html"
-}, {
-    label: "Week 02 Notes",
-    url: "wdd330/week02/index.html"
-}, {
-    label: "Week 03 Notes",
-    url: "wdd330/week03/index.html"
-}, {
-    label: "Week 04 Notes",
-    url: "wdd330/week04/index.html"
-}, {
-    label: "Week 05 Notes",
-    url: "wdd330/week05/index.html"
 }]
 
 function wrapper() {
     // update links
-    for (i = 0; i < links.length; i++) {
+    for (let i = 0; i < links.length; i++) {
 
         // create the li element to hold the anchor tag
         let li = document.createElement('li');
@@ -30,6 +18,6 @@ function wrapper() {
         li.appendChild(a);
 
         // append the li element to the links ol element
-        document.querySelector('ol.links').appendChild(li);
+        document.querySelector('ul.links').appendChild(li);
     }
 }
